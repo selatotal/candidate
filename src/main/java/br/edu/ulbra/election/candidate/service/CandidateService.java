c52f3b4bd6ffbf2d73b33bea64fc04a3a6aafc10
Tales Viegas <tales.viegas@ilegra.com>
2018-11-05 21:43:27 -0200

    Fix messages and identifiers

--- src/main/java/br/edu/ulbra/election/candidate/service/CandidateService.java
+++ src/main/java/br/edu/ulbra/election/candidate/service/CandidateService.java
@@ -22,7 +22,7 @@ public class CandidateService {
     private final ModelMapper modelMapper;
 
     private static final String MESSAGE_INVALID_ID = "Invalid id";
-    private static final String MESSAGE_VOTER_NOT_FOUND = "Candidate not found";
+    private static final String MESSAGE_CANDIDATE_NOT_FOUND = "Candidate not found";
 
     @Autowired
     public CandidateService(CandidateRepository candidateRepository, ModelMapper modelMapper){
@@ -49,7 +49,7 @@ public class CandidateService {
 
         Candidate candidate = candidateRepository.findById(candidateId).orElse(null);
         if (candidate == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+            throw new GenericOutputException(MESSAGE_CANDIDATE_NOT_FOUND);
         }
 
         return modelMapper.map(candidate, CandidateOutput.class);
@@ -63,7 +63,7 @@ public class CandidateService {
 
         Candidate candidate = candidateRepository.findById(candidateId).orElse(null);
         if (candidate == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+            throw new GenericOutputException(MESSAGE_CANDIDATE_NOT_FOUND);
         }
 
         candidate.setElectionId(candidateInput.getElectionId());
@@ -74,14 +74,14 @@ public class CandidateService {
         return modelMapper.map(candidate, CandidateOutput.class);
     }
 
-    public GenericOutput delete(Long voterId) {
-        if (voterId == null){
+    public GenericOutput delete(Long candidateId) {
+        if (candidateId == null){
             throw new GenericOutputException(MESSAGE_INVALID_ID);
         }
 
-        Candidate candidate = candidateRepository.findById(voterId).orElse(null);
+        Candidate candidate = candidateRepository.findById(candidateId).orElse(null);
         if (candidate == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+            throw new GenericOutputException(MESSAGE_CANDIDATE_NOT_FOUND);
         }
 
         candidateRepository.delete(candidate);
